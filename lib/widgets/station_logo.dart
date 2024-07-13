c266f376a3905bad18d9a79e44f1a725e3a2eac1
Sangam Shrestha <2shrestha22@gmail.com>
2024-07-13 11:47:33 +0545

    use cached image

--- lib/widgets/station_logo.dart
+++ lib/widgets/station_logo.dart
@@ -1,3 +1,4 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
 class StationLogo extends StatelessWidget {
@@ -8,14 +9,16 @@ class StationLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: BorderRadius.circular(6),
-        clipBehavior: Clip.hardEdge,
-        child: Image.network(
-          imageUrl,
-          height: 70,
-          width: 70,
-          cacheHeight: 140,
-          cacheWidth: 140,
-        ));
+      borderRadius: BorderRadius.circular(6),
+      clipBehavior: Clip.hardEdge,
+      child: ExtendedImage.network(
+        imageUrl,
+        cache: true,
+        height: 70,
+        width: 70,
+        cacheHeight: 140,
+        cacheWidth: 140,
+      ),
+    );
   }
 }
