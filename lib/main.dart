25295b79bbf98e8bb87391bb6bf51c367778fdef
Sangam Shrestha <2shrestha22@gmail.com>
2024-10-15 11:22:01 +0545

    load url from github

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:radio/app.dart';
 import 'package:radio/models/radio_station.dart';
@@ -32,28 +35,27 @@ Future<void> main() async {
 Future<void> loadStations() async {
   // TODO: add option to load from assets for github in settings page.
 
-  // final remoteUri = Uri.parse(
-  //     'https://cdn.jsdelivr.net/gh/2shrestha22/radio/assets/radio_list.json');
+  final remoteUri = Uri.parse(
+      'https://cdn.jsdelivr.net/gh/2shrestha22/radio/assets/radio_list.json');
 
-  // String data;
-  // if (kDebugMode) {
-  //   // always load from assets in debug mode.
-  //   data = await rootBundle.loadString("assets/radio_list.json");
-  // } else {
-  //   // else get data from github repository
-  //   try {
-  //     data = await http
-  //         .get(remoteUri)
-  //         .timeout(
-  //           const Duration(seconds: 5),
-  //           onTimeout: () => throw const SocketException('Timeout'),
-  //         )
-  //         .then((value) => value.body);
-  //   } catch (_) {
-  //     data = await rootBundle.loadString("assets/radio_list.json");
-  //   }
-  // }
-  final data = await rootBundle.loadString("assets/radio_list.json");
+  String data;
+  if (kDebugMode) {
+    // always load from assets in debug mode.
+    data = await rootBundle.loadString("assets/radio_list.json");
+  } else {
+    // else get data from github repository
+    try {
+      data = await http
+          .get(remoteUri)
+          .timeout(
+            const Duration(seconds: 5),
+            onTimeout: () => throw const SocketException('Timeout'),
+          )
+          .then((value) => value.body);
+    } catch (_) {
+      data = await rootBundle.loadString("assets/radio_list.json");
+    }
+  }
   allRadioStations =
       (jsonDecode(data) as List).map((e) => RadioStation.fromJson(e)).toList();
 }
