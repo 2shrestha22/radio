9c9df8e082949bb306dbaa596cd875d2f7020312
Sangam Shrestha <2shrestha22@gmail.com>
2025-10-11 14:09:31 +0545

    upgrade packages

--- lib/provider/favorite_stations.g.dart
+++ lib/provider/favorite_stations.g.dart
@@ -6,21 +6,55 @@ part of 'favorite_stations.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(FavoriteStations)
+const favoriteStationsProvider = FavoriteStationsProvider._();
+
+final class FavoriteStationsProvider
+    extends $NotifierProvider<FavoriteStations, List<RadioStation>> {
+  const FavoriteStationsProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'favoriteStationsProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$favoriteStationsHash();
+
+  @$internal
+  @override
+  FavoriteStations create() => FavoriteStations();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<RadioStation> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<RadioStation>>(value),
+    );
+  }
+}
+
 String _$favoriteStationsHash() => r'28cd684ad065ce7b567e1dafba57093707af1724';
 
-/// See also [FavoriteStations].
-@ProviderFor(FavoriteStations)
-final favoriteStationsProvider =
-    AutoDisposeNotifierProvider<FavoriteStations, List<RadioStation>>.internal(
-  FavoriteStations.new,
-  name: r'favoriteStationsProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$favoriteStationsHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef _$FavoriteStations = AutoDisposeNotifier<List<RadioStation>>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$FavoriteStations extends $Notifier<List<RadioStation>> {
+  List<RadioStation> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<List<RadioStation>, List<RadioStation>>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<List<RadioStation>, List<RadioStation>>,
+        List<RadioStation>,
+        Object?,
+        Object?>;
+    element.handleValue(ref, created);
+  }
+}
--- lib/provider/search_input.g.dart
+++ lib/provider/search_input.g.dart
@@ -6,20 +6,51 @@ part of 'search_input.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(SearchInput)
+const searchInputProvider = SearchInputProvider._();
+
+final class SearchInputProvider extends $NotifierProvider<SearchInput, String> {
+  const SearchInputProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'searchInputProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$searchInputHash();
+
+  @$internal
+  @override
+  SearchInput create() => SearchInput();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(String value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<String>(value),
+    );
+  }
+}
+
 String _$searchInputHash() => r'1b1d7bce09052f39a4e7fd73a1a52daf36c620b0';
 
-/// See also [SearchInput].
-@ProviderFor(SearchInput)
-final searchInputProvider =
-    AutoDisposeNotifierProvider<SearchInput, String>.internal(
-  SearchInput.new,
-  name: r'searchInputProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$searchInputHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef _$SearchInput = AutoDisposeNotifier<String>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$SearchInput extends $Notifier<String> {
+  String build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<String, String>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<String, String>, String, Object?, Object?>;
+    element.handleValue(ref, created);
+  }
+}
